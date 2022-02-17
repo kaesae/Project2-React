abbe05429a1ed77d29d8cce7ae383288d82099f5
Kae Saetern <kaesaetern@Kaes-MacBook-Pro.local>
2022-02-17 15:44:26 -0800

    components updated

--- src/Components/APIPage.js
+++ src/Components/APIPage.js
@@ -2,7 +2,12 @@
 const APIPage = () => {
     return(
         <div className="APIPage">
-            I'm the API Page
+            This free API was created to keep track of covid cases around the world.
+            The API can show varying statistics such as cases of covid, the amount of 
+            vaccinations and deaths, population, and elevation to name a few.
+
+            The API nests data by country then province/state/region depending on the
+            country.
         </div>
     )
 }
--- src/Components/BioPage.js
+++ src/Components/BioPage.js
@@ -4,9 +4,17 @@ const BioPage = () => {
         <div className="BioPage">
             <p>Hi Friend,</p>
             <p>{' '}</p>
-            <p>As of today, February 17, 2022, we are nearing 2 years of
+            <p>My name is Kae, pronounced kay. As of today, February 17, 2022, we are nearing 2 years of
             the Covid pandemic which has changed the world from sanitation
-            practices to
+            practices to cultural etiquettes. In the city of San Francisco,
+            bars and restaurants, have expanded their spaces to include parklets,
+            moved tables to be 6ft apart, and/or set up physical barriers between 
+            restaurants. Patrons are required to wear masks indoor while ordering,
+            or walking around the establishment. Observationally and culturally, 
+            people have fluxtuated between being more or less inclined to hugs 
+            and handshakes due to the pandemic.
+            <p>{' '}</p>
+            I've 
             </p>
         </div>
     )
