d3419e83e1befcee3ab075cb7a47a01df054946d
Kae Saetern <kaesaetern@Kaes-MacBook-Pro.local>
2022-02-17 16:48:45 -0800

    components updated

--- src/Components/APIPage.js
+++ src/Components/APIPage.js
@@ -2,12 +2,14 @@
 const APIPage = () => {
     return(
         <div className="APIPage">
-            This free API was created to keep track of covid cases around the world.
-            The API can show varying statistics such as cases of covid, the amount of 
-            vaccinations and deaths, population, and elevation to name a few.
+            <div className="APIPageBody">
+                This free API was created to keep track of covid cases around the world.
+                The API can show varying statistics such as cases of covid, the amount of 
+                vaccinations and deaths, population, and elevation to name a few.
 
-            The API nests data by country then province/state/region depending on the
-            country.
+                The API nests data by country then province/state/region depending on the
+                country.
+            </div>
         </div>
     )
 }
--- src/Components/BioPage.js
+++ src/Components/BioPage.js
@@ -2,20 +2,25 @@
 const BioPage = () => {
     return(
         <div className="BioPage">
-            <p>Hi Friend,</p>
-            <p>{' '}</p>
-            <p>My name is Kae, pronounced kay. As of today, February 17, 2022, we are nearing 2 years of
-            the Covid pandemic which has changed the world from sanitation
-            practices to cultural etiquettes. In the city of San Francisco,
-            bars and restaurants, have expanded their spaces to include parklets,
-            moved tables to be 6ft apart, and/or set up physical barriers between 
-            restaurants. Patrons are required to wear masks indoor while ordering,
-            or walking around the establishment. Observationally and culturally, 
-            people have fluxtuated between being more or less inclined to hugs 
-            and handshakes due to the pandemic.
-            <p>{' '}</p>
-            I've 
-            </p>
+            <div className="BioPageBody">
+                <p>Hey there,</p>
+                <p>{' '}</p>
+                <p>My name is Kae, pronounced kay. As of today, February 17, 2022, 
+                we are nearing 2 years of the Covid pandemic which has changed the
+                world from sanitation practices to cultural etiquettes. In the city
+                of San Francisco, bars and restaurants have expanded their spaces to include parklets,
+                moved tables to be 6ft apart, and/or set up physical barriers between 
+                tables. Patrons are required to wear a mask indoor while ordering,
+                or walking around the establishment. Observationally and culturally, 
+                people have fluxtuated between being more or less inclined to hugs 
+                and handshakes due to the pandemic.
+                <p>{' '}</p>
+                I, myself, transformed into the social butterfly I am today as my friends 
+                put it, changed careers, and I've started feeling rooted, making San Francisco
+                my home base. So despite all that has happened, I feel as though I've changed
+                for the better
+                </p>
+            </div>
         </div>
     )
 }
